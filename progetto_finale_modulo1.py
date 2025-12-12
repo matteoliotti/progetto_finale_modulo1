1e0fe12752eec9b6ee7d5e849ca0f77734b90dd0
matteoliotti <matteo260403@gmail.com>
2025-12-12 18:36:00 +0100

    parte 2
    Importazione con Pandas

--- progetto_finale_modulo1.py
+++ progetto_finale_modulo1.py
@@ -5,26 +5,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 
-date=[]
 
-for x in pd.date_range("2024-03-15",periods=6,freq="d"):
-    date.append(x)
-    date.append(x)
-    date.append(x)
-    date.append(x)
-    date.append(x)
+with open("vendite.csv","w") as s:
+    s.write("Data,Negozio,prodotto,quantita,prezzo_unitario\n"
+            "2024-03-15,Roma,smartphone,11,399.99\n"
+            "2024-03-15,Milano,laptop,15,599.99\n"
+            "2024-03-15,Napoli,TV,13,299.99\n"
+            "2024-03-15,Venezia,stereo,11,29.99\n"
+            "2024-03-15,Torino,auricolari,14,9.99\n"
+            "2024-03-16,Roma,radio,16,19.99\n"
+            "2024-03-16,Milano,smartphone,10,399.99\n"
+            "2024-03-16,Napoli,laptop,17,599.99\n"
+            "2024-03-16,Venezia,TV,17,299.99\n"
+            "2024-03-16,Torino,stereo,12,29.99\n"
+            "2024-03-17,Roma,auricolari,14,9.99\n"
+            "2024-03-17,Milano,radio,18,19.99\n"
+            "2024-03-17,Napoli,smartphone,15,399.99\n"
+            "2024-03-17,Venezia,laptop,19,599.99\n"
+            "2024-03-17,Torino,TV,10,299.99\n"
+            "2024-03-18,Roma,stereo,10,29.99\n"
+            "2024-03-18,Milano,auricolari,11,9.99\n"
+            "2024-03-18,Napoli,radio,10,19.99\n"
+            "2024-03-18,Venezia,smartphone,18,399.99\n"
+            "2024-03-18,Torino,laptop,12,599.99\n"
+            "2024-03-19,Roma,TV,15,299.99\n"
+            "2024-03-19,Milano,stereo,16,29.99\n"
+            "2024-03-19,Napoli,auricolari,10,9.99\n"
+            "2024-03-19,Venezia,radio,13,19.99\n"
+            "2024-03-19,Torino,smartphone,20,399.99\n"
+            "2024-03-20,Roma,laptop,10,599.99\n"
+            "2024-03-20,Milano,TV,11,299.99\n"
+            "2024-03-20,Napoli,stereo,16,29.99\n"
+            "2024-03-20,Venezia,auricolari,14,9.99\n"
+            "2024-03-20,Torino,radio,10,19.99\n")
 
-sedi=["Roma","Milano","Napoli","Venezia","Torino"]
-prodotti=["smartphone","laptop","TV","stereo","auricolari","radio",]
-prezzi=[399.99, 599.99, 299.99, 29.99, 9.99, 19.99]
 
-df=pd.DataFrame({
-    "Data":date,
-    "Negozio":sedi*6,
-    "prodotto":prodotti*5,
-    "quantità":np.random.randint(10,21,30),
-    "prezzo unitario":prezzi*5
-})
+# parte 2
 
-df.to_csv("vendite.csv",index=False)
-print(pd.read_csv("vendite.csv"))
+df=pd.DataFrame(pd.read_csv("vendite.csv"))
+print(f"Primo giorno:\n{df.head()}\nforma:{df.shape}\ninfo:{df.info()}")
